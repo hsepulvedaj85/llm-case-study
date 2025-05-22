9c1508805446eb4152f82edf8d58c757d5c63219
Hernan Sepulveda <hsepulvedj@eafit.edu.co>
2025-05-22 08:35:31 -0500

    First version of chunker function

--- src/chunker.py
+++ src/chunker.py
@@ -1,3 +1,4 @@
+# documentar funcion
 ## Extracts text content and splits into chunks
 
 from langchain_community.document_loaders import PyPDFLoader
@@ -9,11 +10,12 @@ def chunk_pdf(file_path: str, chunk_size: int = 500, chunk_overlap: int = 100):
     loader = PyPDFLoader(file_path)
     docs = loader.load()
     print('File loaded...')
+    ## Ingresar preprocesamiento
     
     splitter = RecursiveCharacterTextSplitter(
         chunk_size = chunk_size,
-        chunk_overlap = chunk_overlap
-    )
+        chunk_overlap = chunk_overlap  # Chunking con Solapamiento (Overlap)
+    ) ##Esta clase permite dividir el texto de forma inteligente, tratando de mantener el contexto y la estructura.
     chunks = splitter.split_documents(docs)
     
     # Add page number and source for metadata
